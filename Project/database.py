2ab5ecd48f48a589075fb80963b89b4b2f6b5e9f
scampos37 <scampos37@ucmerced.edu>
2022-11-09 21:35:46 -0800

    Search Specific pokemon base stats
    can use name or number in order to search for a specific pokemon's base stats.

--- Project/database.py
+++ Project/database.py
@@ -162,8 +162,46 @@ def pokemonGenerationSearch(conn):
     except Error as e:
         conn.rollback()
         print(e)
-        
+    print("--------------------------------------------") 
 
+def pokemonBaseStatsSpecific(conn):
+    print("--------------------------------------------")
+    x = ""
+    while(x != "name" or x != "number"):
+        x = input("Search by Pokemon Name (name) or Pokedex Number (number)")
+        if (x == "name"):
+            pname = input("What's the pokemon's name: ")
+            break
+        elif(x == "number"):
+            pdnum = input("What's the pokemon's pokedex number: ")
+            break
+
+    try:
+        if (x == "name") :
+            sql = """SELECT pokemon.pokedex_number, name as pokemon_name, hp, attack, defense, sp_attack, sp_defense, speed, height_m, weight_kg
+                    FROM pokemon, baseStats
+                    WHERE pokemon_name = ?
+                        AND pokemon.pokedex_number = baseStats.pokedex_number"""
+            args = [pname]
+        else:
+            sql = """SELECT pokemon.pokedex_number, name as pokemon_name, hp, attack, defense, sp_attack, sp_defense, speed, height_m, weight_kg
+                    FROM pokemon, baseStats
+                    WHERE pokemon.pokedex_number = ?
+                        AND pokemon.pokedex_number = baseStats.pokedex_number"""
+            args = [pdnum]
+        
+        cur = conn.cursor()
+        cur.execute(sql, args)
+        l = '{:<15} {:<25} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10}'.format("PokedexNumber", "PokemonName", "HP", "attack", "defense", "sp_attack", "sp_defense", "speed", "height (M)", "weight (kg)")
+        print(l)
+        print("-------------------------------")
+        rows = cur.fetchall()
+        for row in rows:
+            l = '{:<15} {:<25} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10}'.format(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9])
+            print(l)
+    except Error as e:
+        conn.rollback()
+        print(e)
     print("--------------------------------------------") 
 
 def main():
@@ -179,8 +217,8 @@ def main():
             #createPokeMovesTable(conn)
             #createPokemonTable(conn)
             #nothing here yet
-            pokemonGenerationSearch(conn)
-
+            #pokemonGenerationSearch(conn)
+            pokemonBaseStatsSpecific(conn)
 
         closeConnection(conn, database)
 
