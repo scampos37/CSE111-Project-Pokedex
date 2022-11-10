fd1babd6c7a877c01edeaee9b068902fd5513b74
Rodolfo J Gutierrez <rgutierrez56@ucmerced.edu>
2022-11-09 21:52:17 -0800

    another query

--- Project/sqlFunctions.py
+++ Project/sqlFunctions.py
@@ -64,3 +64,64 @@ def pokemonBaseStatsSpecific(conn):
         conn.rollback()
         print(e)
     print("--------------------------------------------") 
+
+
+def pokeTypeSearch(conn):
+    print("--------------------------------------------")
+    typeNum = input("What how many typing does the pokemon you are looking for have?: ")
+
+
+    if typeNum == 1:
+        typeName = input("What poke type are you looking for?: ")
+        try:
+            sql = """SELECT name as poke_name, type_1, hp, attack, defense, speed
+                    FROM pokemon, baseStats
+                    WHERE
+                        pokemon.index_number = baseStats.index_number AND
+                        pokemon.pokedex_number = baseStats.poke AND
+                        type_1 = ?"""
+            args = [typeName]
+            cur = conn.cursor()
+            cur.execute(sql, args)
+
+            l = '{:<15} {:<25} {:<10} {:<10} {:<10} {:<10}'.format("Pokemon_Name", "Type", "HP", "Attack", "Defense", "Speed")
+            print(l)
+            print("-------------------------------")
+
+            rows = cur.fetchall()
+            for row in rows:
+                l = '{:<15} {:<25} {:<10} {:<10} {:<10} {:<10}'.format(row[0], row[1], row[2], row[3], row[4], row[5])
+                print(l)
+
+        except Error as e:
+            conn.rollback()
+            print(e)
+
+    elif typeNum == 2:
+         
+        typeName1, typeName2 = input("What poke type are you looking for?: ").split()
+        try:
+            sql = """SELECT name as poke_name, type_1, hp, attack, defense, speed
+                    FROM pokemon, baseStats
+                    WHERE
+                        pokemon.index_number = baseStats.index_number AND
+                        pokemon.pokedex_number = baseStats.poke AND
+                        type_1 = ? AND
+                        type_2 = ?"""
+                        
+            args = [typeName1, typeName2]
+            cur = conn.cursor()
+            cur.execute(sql, args)
+
+            l = '{:<15} {:<25} {:<10} {:<10} {:<10} {:<10} {:<10}'.format("Pokemon_Name", "Type1", "Type2", "HP", "Attack", "Defense", "Speed")
+            print(l)
+            print("-------------------------------")
+
+            rows = cur.fetchall()
+            for row in rows:
+                l = '{:<15} {:<25} {:<10} {:<10} {:<10} {:<10} {:<10}'.format(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
+                print(l)
+
+        except Error as e:
+            conn.rollback()
+            print(e)
